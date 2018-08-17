3e2f5be5b6ed1d4754ea409fb7782b633766411d
AssistantsCOP <40172346+AssistantsCOP@users.noreply.github.com>
2018-08-17 12:52:23 +0000

    Update labRequests.js

--- labRequests.js
+++ labRequests.js
@@ -54,4 +54,5 @@ var labEvents = [
   { title: 'Tosca Robotics Exchange / Martin S.',    start: '2018-09-27T14:00:00', end: '2018-09-27T16:00:00',    resourceId: 'B-LONGTABLE'    },
   { title: 'COP Offsite Meeting / Armin B.',   start: '2018-10-11', end: '2018-10-13',    resourceId: 'C-MEETROOM'     },
   { title: 'ExCo Lunch / Konstanze O.',  start: '2018-12-06T10:00:00', end: '2018-12-06T15:00:00',     resourceId: 'C-MEETROOM' }
+  { title: 'COP OPS Meeting / COP Assistants',    start: '2018-09-19T14:00:00', end: '2018-09-19T15:00:00',    resourceId: 'B-LONGTABLE'    
 ]
